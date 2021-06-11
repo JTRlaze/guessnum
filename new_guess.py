e2bddc78c75a9f77cbb640e95dfee3aa1ae31a9e
JTRlaze <s107270323@gmail.com>
2021-06-11 18:59:56 +0800

    whole guess game

--- new_guess.py
+++ new_guess.py
@@ -1,11 +1,16 @@
-# 產生一個隨機整數1~100
+# 產生一個隨機整數
 # 讓使用者重複輸入數字去猜
 # 猜對的話 印出"終於猜對了!"
 # 猜對的話 要告訴他 比答案大/小
 
 
 import random
-r = random.randint(1, 100)
+start = input('請決定隨機數字範圍開始值:')
+end = input('請決定隨機數字範圍結束值:')
+start = int(start)
+end = int(end)
+
+r = random.randint(start, end)
 count = 0
 while True:
     count = count + 1
